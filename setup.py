302af3f64389acedf5e1d2f0b27d54e041760ebd
ShadowMoose <theshadowmoose@gmail.com>
2018-05-26 16:03:34 -0700

    Initial files.

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from setuptools import setup
 
 
-def readme():
-	with open('README.md') as f:
-		return f.read()
+def readme(file='', split=False):
+	with open(file) as f:
+		if split:
+			return f.readlines()
+		else:
+			return f.read()
 
 
 setup(
 	name='chromedriver_install',
 	version='0.1',
-	description='Installs chromedriver automatically.',
-	long_description=readme(),
+	description='Package for installing the latest chromedriver automatically.',
+	long_description=readme('README.md'),
 	url='http://github.com/shadowmoose/chrome_driver',
 	author='ShadowMoose',
 	author_email='shadowmoose@github.com',
 	license='MIT',
 	packages=['chromedriver_install'],
-	install_requires=[
-		'requests',
-	],
+	install_requires=readme('requirements.txt', split=True),
 	entry_points={
-		'console_scripts': ['chromedriver-install=chromedriver_install:install'],
+		'console_scripts': ['install-chromedriver=chromedriver_install:install'],
 	},
 	zip_safe=False)
