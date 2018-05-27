176bd043aa375438448023ac04b6e33c1ce55ae7
ShadowMoose <theshadowmoose@gmail.com>
2018-05-26 17:16:01 -0700

    Make all variables private.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def readme(file='', split=False):
 
 setup(
 	name='chromedriver_install',
-	version='0.2',
+	version='0.3',
 	description='Package for installing the latest chromedriver automatically.',
 	long_description=readme('README.md'),
 	url='http://github.com/shadowmoose/chrome_driver',
