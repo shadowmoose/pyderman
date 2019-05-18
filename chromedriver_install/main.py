5bd6831eb9d337d67867144ae2191af9e35b639c
ShadowMoose <theshadowmoose@gmail.com>
2019-05-18 12:57:14 -0700

    Add test version check.

--- chromedriver_install/main.py
+++ chromedriver_install/main.py
@@ -1,11 +1,15 @@
 import os
 from chromedriver_install import install
+import subprocess
 
 
 def run():
 	path = install(verbose=True, chmod=True)
 	if not os.path.exists(path):
 		raise FileNotFoundError('The chromedriver executable was not properly downloaded.')
+	output = subprocess.check_output([path, '-version']).decode('utf-8')
+	print('Version:', output)
+	assert 'chromedriver' in output.lower()
 	print('Chromedriver is installed at: "%s"' % path)
 
 
