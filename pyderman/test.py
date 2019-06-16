60cfb8bb76cf4c49851d1fa4d61b8729e69fba1b
ShadowMoose <theshadowmoose@gmail.com>
2019-06-15 20:30:53 -0700

    Shifted to proper unit test.

--- pyderman/test.py
+++ pyderman/test.py
@@ -1,24 +1,27 @@
 import os
 from pyderman import install, all_drivers
 import subprocess
+import unittest
 
 
-def run():
-	for driver in all_drivers:
-		print("Testing %s..." % driver.__name__)
-		data = install(browser=driver, verbose=True, chmod=True, overwrite=True, return_info=True)
-		path = data['path']
-		if not os.path.exists(path):
-			raise FileNotFoundError('The %s executable was not properly downloaded.' % driver.__name__)
-		output = subprocess.check_output([path, '--version']).decode('utf-8')
-		print('Version:', output)
-		assert data['version'] in output.lower()
-		print('%s is installed at: "%s"' % (data['driver'], path))
-		print('\n\n\n')
+class TestStringMethods(unittest.TestCase):
+
+	def test_upper(self):
+		for driver in all_drivers:
+			print("Testing %s..." % driver.__name__)
+			data = install(browser=driver, verbose=True, chmod=True, overwrite=True, return_info=True)
+			path = data['path']
+			if not os.path.exists(path):
+				raise FileNotFoundError('The %s executable was not properly downloaded.' % driver.__name__)
+			output = subprocess.check_output([path, '--version']).decode('utf-8')
+			print('Version:', output)
+			self.assertIn(data['version'], output.lower(), "Driver %s did not output proper version! ('%s')" % (driver.__name__, data['version']))
+			print('%s is installed at: "%s"' % (data['driver'], path))
+			print('\n\n\n')
 
 
 if __name__ == "__main__":
-	run()
+	unittest.main()
 
 
 """
