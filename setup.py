acc5713b0f37745084073355c43d8efc92a1e236
ShadowMoose <theshadowmoose@gmail.com>
2019-06-16 00:37:31 -0700

    Update project description.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme(file='', split=False):
 setup(
 	name='pyderman',
 	version='1.2.0',
-	description='Installs the latest Chrome/Firefox/Gecko web drivers automatically.',
+	description='Installs the latest Chrome/Firefox/Opera web drivers automatically.',
 	long_description=readme('README.md'),
 	long_description_content_type='text/markdown',
 	url='http://github.com/shadowmoose/pyderman',
