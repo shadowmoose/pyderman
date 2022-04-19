1864b6d0ca9cee060312373b88cf6b8574474e24
eggplants <w10776e8w@yahoo.co.jp>
2022-04-20 06:04:18 +0900

    fix: follow mypy strictly

--- pyderman/drivers/edge.py
+++ pyderman/drivers/edge.py
@@ -1,33 +1,51 @@
-from pyderman.util import downloader
+from __future__ import annotations
+
 import re
 
-
-def get_url(version='latest', _os=None, _os_bit=None):
-	if version == 'latest':
-		try:
-			version = latest()
-		except Exception as e:
-			print(e)
-	if _os == 'win':
-		url = 'https://msedgedriver.azureedge.net/%s/edgedriver_win%s.zip' % (version, _os_bit)
-	elif _os == 'mac':
-		url = 'https://msedgedriver.azureedge.net/%s/edgedriver_mac64.zip' % version
-	else:
-		raise OSError('There is no valid EdgeDriver release for %s' % _os)
-	if not version:
-		raise ValueError('Unable to automatically locate latest version of EdgeDriver!')
-	return 'msedgedriver', url, version
+from pyderman.util import downloader
 
 
-def latest():
-	url = 'https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/'
-	data = downloader.raw(url)
-	regex = r"https://msedgedriver\.azureedge\.net/(.+?)/edgedriver"
-	matches = list(set([m.group(1) for m in re.finditer(regex, data, re.MULTILINE)]))
-	matches = [m for m in matches if m.replace('.', '').isnumeric()]
-	matches.sort(key=lambda s: [int(u) for u in s.split('.')], reverse=True)
-	return matches[0]
+def get_url(
+    version: str = "latest", _os: str | None = None, _os_bit: str | None = None
+) -> tuple[str, str, str]:
+    if version == "latest":
+        try:
+            version = latest()
+        except Exception as e:
+            print(e)
+    if _os == "win":
+        url = "https://msedgedriver.azureedge.net/%s/edgedriver_win%s.zip" % (
+            version,
+            _os_bit,
+        )
+    elif _os == "mac":
+        url = "https://msedgedriver.azureedge.net/%s/edgedriver_mac64.zip" % version
+    else:
+        raise OSError("There is no valid EdgeDriver release for %s" % _os)
+    if not version:
+        raise ValueError("Unable to automatically locate latest version of EdgeDriver!")
+    return "msedgedriver", url, version
+
+
+def latest() -> str:
+    url = "https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/"
+    data = downloader.raw(url)
+    if data is None:
+        raise Exception("Unable to get: %s" % url)
+    regex = r"https://msedgedriver\.azureedge\.net/(.+?)/edgedriver"
+    matches = list(
+        set(
+            [
+                str(m.group(1))
+                for m in re.finditer(regex, data, re.MULTILINE)
+                if type(m) is re.Match
+            ]
+        )
+    )
+    matches = [m for m in matches if m.replace(".", "").isnumeric()]
+    matches.sort(key=lambda s: [int(u) for u in s.split(".")], reverse=True)
+    return matches[0]
 
 
 if __name__ == "__main__":
-	print(get_url('latest', 'win', '64'))
+    print(get_url("latest", "win", "64"))
--- pyderman/drivers/phantomjs.py
+++ pyderman/drivers/phantomjs.py
@@ -1,34 +1,50 @@
-from pyderman.util import downloader
-import re
+from __future__ import annotations
+
 import json
+import re
+from typing import Any, Generator
+
+from pyderman.util import downloader
 
 
-def get_url(version='latest', _os=None, _os_bit=None):
-	beta = True
-	pattern = version
-	bit = ''
-	if not version or version == 'latest':
-		beta = False
-		pattern = ''
-	if _os == 'linux':
-		bit = '64' if _os_bit == '64' else 'i686'
-	for release in _releases():
-		name = release['name'].lower()
-		if not beta and 'beta' in name:
-			continue
-		if _os in name and pattern in name and bit in name:
-			ver = re.search(r'(\d{1,2}\.\d{1,2}\.\d{1,2})', name).group(1)
-			return 'phantomjs.*/bin/phantomjs', release['links']['self']['href'], ver
+def get_url(
+    version: str = "latest", _os: str | None = None, _os_bit: str | None = None
+) -> tuple[str, str, str]:
+    beta = True
+    pattern = version
+    bit = ""
+    if not version or version == "latest":
+        beta = False
+        pattern = ""
+    if _os == "linux":
+        bit = "64" if _os_bit == "64" else "i686"
+    for release in _releases():
+        name = str(release["name"]).lower()
+        if not beta and "beta" in name:
+            continue
+        if _os is not None and _os in name and pattern in name and bit in name:
+            ver = re.search(r"(\d{1,2}\.\d{1,2}\.\d{1,2})", name)
+            if ver is not None:
+                return (
+                    "phantomjs.*/bin/phantomjs",
+                    release["links"]["self"]["href"],
+                    str(ver.group(1)),
+                )
+    raise ValueError("Unable to get url")
 
 
-def _releases():
-	page = 'https://api.bitbucket.org/2.0/repositories/ariya/phantomjs/downloads/'
-	while page:
-		data = json.loads(downloader.raw(page))
-		for release in data['values']:
-			yield release
-		page = data['next'] if 'next' in data else None
+def _releases() -> Generator[dict[str, Any], None, None]:
+    page = "https://api.bitbucket.org/2.0/repositories/ariya/phantomjs/downloads/"
+    while page:
+        s = downloader.raw(page)
+        if s is None:
+            raise ValueError("Unable to get page: %s" % page)
+        else:
+            data = json.loads(s)
+            for release in data["values"]:
+                yield release
+            page = data["next"] if "next" in data else None
 
 
 if __name__ == "__main__":
-	print(get_url('latest', 'win', '64'))
+    print(get_url("latest", "win", "64"))
